cccc6f74e3e752ab0ca7e3c9a259d3d01e3762e6
Rob Ungar <robungar@gmail.com>
2017-04-21 19:40:38 -0400

    email sender utility class

--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var helper = require('sendgrid').mail;
+var utils = require('../utils')
+//var helper = require('sendgrid').mail;
 
 
 router.post('/:action', function(req, res, next) {
 	var action = req.params.action
 
 	if(action == 'send'){
-		var from_email = new helper.Email('robungar@gmail.com');
-		var to_email = new helper.Email(req.body.recipient);
-		var subject = req.body.subject;
-		var content = new helper.Content('text/html', req.body.content);
-		var mail = new helper.Mail(from_email, subject, to_email, content)
+		var recipients = req.body.recipients
+		var list = recipients.split(',')
 
-		var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-		var request = sg.emptyRequest({
-		  method: 'POST',
-		  path: '/v3/mail/send',
-		  body: mail.toJSON(),
-		});
-
-		sg.API(request, function(error, response) {
-			if(error){
-				res.json({
-					confirmation: 'fail',
-					message: error
-				})
-				return
-			}
+		utils.Email.sendEmails(list, req.body, function(){
 			res.json({
-		  	confirmation: 'success',
-		  	response: response
-		  })
+				confirmation: 'success',
+				message: 'Emails Sent!'
+			})
+		})
+			
+	// 	utils.Email
+	// 	.sendEmail(req.body)
+	// 	.then(function(response){
+	// 		res.json({
+	// 			confirmation: 'success',
+	// 			response: response
+	// 		})
+	// 	})
+	// 	.catch(function(err){
+	// 		res.json({
+	// 			confirmation: 'fail',
+	// 			message: err
+	// 		})
+	// 	})
 	
-		});
 		return 
 	}
 
@@ -44,48 +43,5 @@ router.post('/:action', function(req, res, next) {
 })
 
 
-
-/* GET users listing. */
-router.get('/:action', function(req, res, next) {
-	var action = req.params.action
-
-	if(action == 'send'){
-		var from_email = new helper.Email('robungar@gmail.com');
-		var to_email = new helper.Email('robungar@gmail.com');
-		var subject = 'TEST!';
-		var content = new helper.Content('text/html', 'Hello, From Email Dispatch!');
-		var mail = new helper.Mail(from_email, subject, to_email, content)
-
-		var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-		var request = sg.emptyRequest({
-		  method: 'POST',
-		  path: '/v3/mail/send',
-		  body: mail.toJSON(),
-		});
-
-		sg.API(request, function(error, response) {
-			if(error){
-				res.json({
-					confirmation: 'fail',
-					message: error
-				})
-				return
-			}
-			res.json({
-		  	confirmation: 'success',
-		  	response: response
-		  })
-	
-		});
-
-		return 
-	}
-
-	res.json({
-  	confirmation: 'fail',
-  	message: 'Invalid Action'
-  })
-  
-});
 
 module.exports = router;
