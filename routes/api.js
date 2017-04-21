01857afa1cd4f138c5be2993569cc9a006e4a83a
Rob Ungar <robungar@gmail.com>
2017-04-21 19:14:16 -0400

    send email through post request

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,50 @@ var router = express.Router();
 var helper = require('sendgrid').mail;
 
 
-/* GET users listing. */
 router.post('/:action', function(req, res, next) {
+	var action = req.params.action
+
+	if(action == 'send'){
+		var from_email = new helper.Email('robungar@gmail.com');
+		var to_email = new helper.Email(req.body.recipient);
+		var subject = req.body.subject;
+		var content = new helper.Content('text/html', req.body.content);
+		var mail = new helper.Mail(from_email, subject, to_email, content)
+
+		var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+		var request = sg.emptyRequest({
+		  method: 'POST',
+		  path: '/v3/mail/send',
+		  body: mail.toJSON(),
+		});
+
+		sg.API(request, function(error, response) {
+			if(error){
+				res.json({
+					confirmation: 'fail',
+					message: error
+				})
+				return
+			}
+			res.json({
+		  	confirmation: 'success',
+		  	response: response
+		  })
+	
+		});
+		return 
+	}
+
+	res.json({
+  	confirmation: 'fail',
+  	message: 'Invalid Action'
+  })
+})
+
+
+
+/* GET users listing. */
+router.get('/:action', function(req, res, next) {
 	var action = req.params.action
 
 	if(action == 'send'){
@@ -36,7 +78,6 @@ router.post('/:action', function(req, res, next) {
 	
 		});
 
-		
 		return 
 	}
 
