5a962488a6d4b3e6a966612ded564d238aaad960
Rob Ungar <robungar@gmail.com>
2017-04-21 18:48:27 -0400

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/:action', function(req, res, next) {
+	var action = req.params.action
+
+	if(action == 'send'){
+		res.json({
+	  	confirmation: 'success',
+	  	action: action
+	  })
+		return 
+	}
+
+	res.json({
+  	confirmation: 'fail',
+  	message: 'Invalid Action'
+  })
+  
+});
+
+module.exports = router;
